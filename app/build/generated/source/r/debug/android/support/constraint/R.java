cb57c7c1bb7463f6a0fea3c89bfb7ecff6c52422
Mohamed Shahin <M7edshin@hotmail.com>
2018-04-20 16:29:00 +0100

    Implementing Tablet Mode

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -69,23 +69,23 @@ public final class R {
     public static final class id {
         public static final int barrier = 0x7f07001e;
         public static final int bottom = 0x7f070021;
-        public static final int chains = 0x7f070027;
-        public static final int dimensions = 0x7f070032;
-        public static final int direct = 0x7f070033;
-        public static final int end = 0x7f070036;
-        public static final int gone = 0x7f07003e;
-        public static final int invisible = 0x7f070046;
-        public static final int left = 0x7f070049;
-        public static final int none = 0x7f070053;
-        public static final int packed = 0x7f070058;
-        public static final int parent = 0x7f070059;
-        public static final int percent = 0x7f07005b;
-        public static final int right = 0x7f07005f;
-        public static final int spread = 0x7f070079;
-        public static final int spread_inside = 0x7f07007a;
-        public static final int standard = 0x7f07007e;
-        public static final int start = 0x7f07007f;
-        public static final int top = 0x7f07008e;
+        public static final int chains = 0x7f070026;
+        public static final int dimensions = 0x7f070031;
+        public static final int direct = 0x7f070032;
+        public static final int end = 0x7f070035;
+        public static final int gone = 0x7f07003d;
+        public static final int invisible = 0x7f070045;
+        public static final int left = 0x7f070048;
+        public static final int none = 0x7f070052;
+        public static final int packed = 0x7f070057;
+        public static final int parent = 0x7f070058;
+        public static final int percent = 0x7f07005a;
+        public static final int right = 0x7f07005e;
+        public static final int spread = 0x7f070078;
+        public static final int spread_inside = 0x7f070079;
+        public static final int standard = 0x7f07007d;
+        public static final int start = 0x7f07007e;
+        public static final int top = 0x7f07008d;
         public static final int wrap = 0x7f070097;
     }
     public static final class styleable {
